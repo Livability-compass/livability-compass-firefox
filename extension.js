3cdc9998fe3d506e5df0260a1882c98c97ce9695
Bradley Oosterveen <BradleyOosterveen@gmail.com>
2024-05-11 17:27:02 +0200

    Edit manifest

--- extension.js
+++ extension.js
@@ -1,3 +1,5 @@
+// Photo by <a href="https://unsplash.com/@esthergrl1?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Esther Gorlee</a> on <a href="https://unsplash.com/photos/red-tulip-flowers-under-calm-blue-sky--uGmFjqkHFU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
 (async () => {
   let buurt = document.getElementsByClassName(
     "ml-2 text-secondary-70 hover:text-secondary-70-darken-1"
